b1bad8a0591699bd002cfb5ef1108367eda4b934
Hugo Lopes Tavares <hltbra@gmail.com>
2011-11-03 13:49:32 -0200

    Finally get config param working!

--- ngx_http_strftime_module.c
+++ ngx_http_strftime_module.c
@@ -57,10 +57,17 @@ ngx_http_strftime(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
     ngx_str_t var_name;
     ngx_http_variable_t *v;
     ngx_str_t *params;
+    ngx_http_strftime_loc_conf_t  *ltcf = conf;
 
     params = cf->args->elts;
     var_name.data = params[1].data;
     var_name.len = params[1].len;
+
+    ltcf->date_fmt = ngx_palloc(cf->pool, params[2].len);
+    if (ltcf->date_fmt == NULL) {
+        return NGX_ERROR;
+    }
+    ngx_memcpy(ltcf->date_fmt, params[2].data, params[2].len);
  
     v = ngx_http_add_variable(cf, &var_name, NGX_HTTP_VAR_NOCACHEABLE);
     v->get_handler = var_get_handler;
@@ -71,7 +78,7 @@ static ngx_command_t ngx_http_strftime_commands[] = {
     {ngx_string("strftime"),
      NGX_HTTP_LOC_CONF|NGX_CONF_TAKE2,
      ngx_http_strftime,
-     0,
+     NGX_HTTP_LOC_CONF_OFFSET,
      0,
      NULL
     },

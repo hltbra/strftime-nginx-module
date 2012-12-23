15d5d71c611ef3ec6cb0457c42b0da81732b6afa
Wandenberg Peixoto <wandenberg@gmail.com>
2012-12-23 12:41:58 -0200

    enable use the strftime directive in any place

--- ngx_http_strftime_module.c
+++ ngx_http_strftime_module.c
@@ -68,7 +68,7 @@ ngx_http_strftime(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
 
 static ngx_command_t ngx_http_strftime_commands[] = {
     {ngx_string("strftime"),
-     NGX_HTTP_LOC_CONF|NGX_CONF_TAKE2,
+     NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE2,
      ngx_http_strftime,
      0,
      0,

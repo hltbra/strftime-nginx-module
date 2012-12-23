25027200661fb6d38edb70875a56667eab722f2b
Wandenberg Peixoto <wandenberg@gmail.com>
2012-12-23 12:51:07 -0200

    adding an option to use gmt or local time

--- ngx_http_strftime_module.c
+++ ngx_http_strftime_module.c
@@ -3,18 +3,20 @@
 #include <ngx_http.h>
 #define DATE_MAX_LEN 30
 
+
 typedef struct {
-    u_char *date_fmt;
+    ngx_uint_t    gmt;
+    u_char       *date_fmt;
 } ngx_http_strftime_ctx_t;
 
 
 static char *ngx_http_strftime(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
-ngx_int_t    var_get_handler(ngx_http_request_t *r, ngx_http_variable_value_t *v, uintptr_t data);
+ngx_int_t    ngx_http_strftime_time_variable(ngx_http_request_t *r, ngx_http_variable_value_t *v, uintptr_t data);
 
 
 static ngx_command_t ngx_http_strftime_commands[] = {
     {ngx_string("strftime"),
-     NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE2,
+     NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE23,
      ngx_http_strftime,
      0,
      0,
@@ -58,44 +60,64 @@ ngx_module_t ngx_http_strftime_module = {
 static char *
 ngx_http_strftime(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
     ngx_http_strftime_ctx_t *ctx;
-    ngx_str_t var_name;
-    ngx_http_variable_t *v;
-    ngx_str_t *params;
+    ngx_http_variable_t     *v;
+    ngx_str_t               *args;
+    ngx_str_t                var_name;
+    ngx_str_t                date_fmt;
+    ngx_str_t                zone = ngx_string("local");
 
-    params = cf->args->elts;
-    var_name.data = params[1].data;
-    var_name.len = params[1].len;
+    if (cf->args->nelts < 2) {
+        ngx_conf_log_error(NGX_LOG_ERR, cf, 0, "Incorrect use of strftime. Try 'strftime variable_name date_time_format [local|gmt (local is the default)]'");
+        return NGX_CONF_ERROR;
+    }
+
+    args = (ngx_str_t *) cf->args->elts;
+    var_name = args[1];
+    date_fmt = args[2];
+    if (cf->args->nelts > 3) {
+        zone = args[3];
+    }
 
     ctx = ngx_palloc(cf->pool, sizeof(ngx_http_strftime_ctx_t));
     if (ctx == NULL) {
         return NGX_CONF_ERROR;
     }
 
-    ctx->date_fmt = ngx_palloc(cf->pool, params[2].len + 1);
+    ctx->date_fmt = ngx_palloc(cf->pool, date_fmt.len + 1);
     if (ctx->date_fmt == NULL) {
         return NGX_CONF_ERROR;
     }
-    ngx_memcpy(ctx->date_fmt, params[2].data, params[2].len);
-    ctx->date_fmt[params[2].len] = '\0';
+
+    ctx->gmt = ((zone.len == 3) && (ngx_strncasecmp(zone.data, (u_char *) "gmt", 3) == 0));
+    ngx_memcpy(ctx->date_fmt, date_fmt.data, date_fmt.len);
+    ctx->date_fmt[date_fmt.len] = '\0';
 
     v = ngx_http_add_variable(cf, &var_name, NGX_HTTP_VAR_NOCACHEABLE);
-    v->get_handler = var_get_handler;
+    v->get_handler = ngx_http_strftime_time_variable;
     v->data = (uintptr_t) ctx;
+
     return NGX_CONF_OK;
 }
 
 
 ngx_int_t
-strftime_now(ngx_http_variable_value_t *var, u_char *date_fmt, ngx_pool_t *pool)
+strftime_now(ngx_http_variable_value_t *var, u_char *date_fmt, ngx_uint_t gmt, ngx_pool_t *pool)
 {
-    struct tm *ptr;
-    time_t now;
-    char buf[DATE_MAX_LEN];
+    struct tm    tm;
+    time_t       now = ngx_time();
+    char         buf[DATE_MAX_LEN];
 
-    now = time(NULL);
-    ptr = localtime(&now);
+    if (var->len != 0) {
+        return NGX_OK;
+    }
 
-    var->len = strftime(buf, DATE_MAX_LEN, (char *)date_fmt, ptr);
+    if (gmt) {
+        ngx_libc_gmtime(now, &tm);
+    } else {
+        ngx_libc_localtime(now, &tm);
+    }
+
+    var->len = strftime(buf, DATE_MAX_LEN, (char *) date_fmt, &tm);
     if (var->len == 0) {
         return NGX_ERROR;
     }
@@ -106,13 +128,17 @@ strftime_now(ngx_http_variable_value_t *var, u_char *date_fmt, ngx_pool_t *pool)
     }
 
     ngx_memcpy(var->data, buf, var->len);
+    var->valid = 1;
+    var->no_cacheable = 0;
+    var->not_found = 0;
+
     return NGX_OK;
 }
 
 
 ngx_int_t
-var_get_handler(ngx_http_request_t *r, ngx_http_variable_value_t *var, uintptr_t data)
+ngx_http_strftime_time_variable(ngx_http_request_t *r, ngx_http_variable_value_t *var, uintptr_t data)
 {
     ngx_http_strftime_ctx_t *ctx = (ngx_http_strftime_ctx_t *) data;
-    return strftime_now(var, ctx->date_fmt, r->pool);
+    return strftime_now(var, ctx->date_fmt, ctx->gmt, r->pool);
 }
